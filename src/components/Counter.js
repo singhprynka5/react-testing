3682889a414d7508f72735c7dad5285a49bf56e5
Priyanka Singh <singhprynka5@gmail.com>
2024-01-31 16:16:06 +0530

    test setup

---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
+  return (
+    <div className="container">
+      <button data-testid="increment" onClick={incrementCounter}>
+        +
+      </button>
+      <p data-testid="counter">{counter}</p>
+      <button data-testid="decrement" onClick={decrementCounter}>
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
